08116ff0139205659ee884e20d1b6ff1d90795ce
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:59:28 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,5 +1,5 @@
 return {
-    ["Sudoku"] = { fr = "Sudoku" },
-    ["Daily Challenge"] = { fr = "Défi du jour" },
-    ["Daily Challenge \xE2\x9C\x93"] = { fr = "Défi du jour \xE2\x9C\x93" },
+    ["Sudoku"] = { fr = "Sudoku", es = "Sudoku", de = "Sudoku" },
+    ["Daily Challenge"] = { fr = "Défi du jour", es = "Desafío diario", de = "Tägliche Herausforderung" },
+    ["Daily Challenge \xE2\x9C\x93"] = { fr = "Défi du jour \xE2\x9C\x93", es = "Desafío diario \xE2\x9C\x93", de = "Tägliche Herausforderung \xE2\x9C\x93" },
 }
